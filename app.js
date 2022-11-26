2138f7cafd48ae8320baf16354570ccaad89c576
Marcelo Mechi <marcedumechi@gmail.com>
2022-11-26 19:43:38 -0300

    HTML and JS updates

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const winningScoreSelect = document.querySelector('#play-to');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 //PICK WINNING SCORE
@@ -23,8 +23,10 @@ function resetScore() {
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
 
 resetBtn.addEventListener('click', resetScore);
@@ -35,8 +37,10 @@ p1Button.addEventListener('click', () => {
         p1Score++;
         if (p1Score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Score;
     }
@@ -47,8 +51,10 @@ p2Button.addEventListener('click', () => {
         p2Score++;
         if (p2Score === winningScore) {
             isGameOver = true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
